7c03fd375a9cf78e8209e7b5f386f2ab05eaebb4
Snigdha Singh <62167899+snigdha920@users.noreply.github.com>
2021-03-19 23:49:57 +0530

    Course Schedule II

---
+++ Course Schedule II.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    // state - 1: visited, 2: visiting, 0: not touched
+    unordered_map<int, int> state;
+    vector<vector<int>> graph;
+    vector<int> result;
+    void createGraph(int numCourses, vector<vector<int>>& prerequisites) {
+        for(int i=0; i<numCourses; i++) {
+           state[i] = 0;
+        }
+        for(int i=0; i<prerequisites.size(); i++) {
+            int first = prerequisites[i][0];
+            int second = prerequisites[i][1];
+            graph[first].push_back(second);
+        }
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        graph.resize(numCourses+1, vector<int>());
+        createGraph(numCourses, prerequisites);
+        if(doDfs(numCourses)) return result;
+        else return vector<int>();
+    }
+    bool doDfs(int numCourses) {
+        for(int i=0; i<numCourses; i++) {
+            if(dfs(i) == false) return false;
+        }
+        return true;
+    }
+    bool dfs(int vertex) {
+        if(state[vertex] == 1) return true;
+        state[vertex] = 2;
+        for(int i=0; i<graph[vertex].size(); i++) {
+            if(state[graph[vertex][i]] == 2) {
+                return false;
+            }
+            if(dfs(graph[vertex][i]) == false) return false; 
+        }
+        result.push_back(vertex);
+        state[vertex] = 1;
+        return true;
+    }
+};
